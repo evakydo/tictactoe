ba4085cbb71403933359afe1a6e9e87892c6e344
EVE\evakydo <eva.kydonaki@oramavr.com>
2025-05-05 23:10:08 +0300

    AI move logic

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,3 +1,5 @@
+const { winningCombinations } = require('./constants');
+
 exports.handleGameState = (req, res) => {
   const { index } = req.body;
 
@@ -5,46 +7,40 @@ exports.handleGameState = (req, res) => {
     return res.status(400).json({ message: 'fail', reason: 'Invalid index' });
   }
 
-  console.log('Controller received cell index:' + index);
-
   res.json({ message: 'success' });
 };
 
 exports.playerMove = (req, res) => {
   const { index, boardState } = req.body;
-  checkGameState(boardState,'player')
-  res.json({ index: index+1 });
+  const ongoingGame = checkGameState(boardState,'player1')
+  if(ongoingGame)
+    res.json({ index: index+1 });
+  else
+    res.json({ index: -1 });
 };
 
 exports.AIMove = (req, res) => {
-  const { index, boardState } = req.body;
-  res.json({ index: index+1 });
+  const { boardState } = req.body;
+  let backupIndex;
+  for (const combo of winningCombinations) {
+    const emptyIndices = combo.filter(index => boardState[index] === null);
+    const aIIndices = combo.filter(index => boardState[index] === 'player2');
+    const playerIndices = combo.filter(index => boardState[index] === 'player1');
+
+    if (emptyIndices.length === 0) continue;
+    backupIndex = emptyIndices[0];
+    if (aIIndices.length >= 1 && playerIndices.length === 0) 
+      return res.json({ index: emptyIndices[0]});
+  }
+  res.json({ index: backupIndex});
 };
 
 function checkGameState(boardState, player) {
-  //winning combinations
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
   var winner =  winningCombinations.some(combination =>
     combination.every(index => boardState[index] === player)
   );
-  console.log(player)
   if(winner)
-    console.log(player + 'has won')
+    return false;
   else
-    console.log('ongoing game')
-  boardState.forEach(cell => {
-    // if(!cell) {
-    //   return false;
-    // }
-  });
-  return true;
+    return true;
 }
